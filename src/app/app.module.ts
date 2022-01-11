fa5fc0f393c2eb530e5e0f82a1299e4a2a165176
Dipika Patil <DPatil@tql.com>
2022-01-11 10:15:22 +0530

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'  
 import { ReactiveFormsModule} from '@angular/forms' 
 import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatFormFieldModule } from '@angular/material/form-field'
+import {MatNativeDateModule} from '@angular/material/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+
 
 import { AppComponent } from './app.component';
 import { CustomerDetailsComponent } from './customer-details/customer-details.component';
@@ -19,9 +26,19 @@ import { CustomerService } from './customer.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    BrowserAnimationsModule,
+    MatInputModule
+
+ 
+   
+     
   ],
-  providers: [HttpClientModule,CustomerService],
+  providers: [HttpClientModule,CustomerService,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
